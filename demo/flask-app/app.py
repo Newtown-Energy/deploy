d6c865e4cd19ee253c6898e22e29672281e7c3a6
James Vasile <james@jamesvasile.com>
2025-06-30 09:10:18 -0400

    Add demo

---
+++ demo/flask-app/app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/env')
+def show_env():
+    return jsonify({
+        "SMTP_HOST": os.environ.get("SMTP_HOST"),
+        "SMTP_USER": os.environ.get("SMTP_USER"),
+        "SMTP_FROM_ADDRESS": os.environ.get("SMTP_FROM_ADDRESS"),
+        "HOME": os.environ.get("HOME"),
+    })
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
